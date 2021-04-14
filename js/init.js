82970ab5d05a4f5fd52f41d696aabb82ed481286
Coral42 <82488154+Coral42@users.noreply.github.com>
2021-04-13 22:35:08 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAVf1LU1Fe9Mr8--LGy3PGRar5ynifH-wk",
+    authDomain: "chat42cs.firebaseapp.com",
+    projectId: "chat42cs",
+    storageBucket: "chat42cs.appspot.com",
+    messagingSenderId: "351775557816",
+    appId: "1:351775557816:web:813c6008f4b1e4e2cb4c4d",
+    measurementId: "G-R7SE0NGVNC"
+    });
