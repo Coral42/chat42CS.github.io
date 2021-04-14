0660ed941f53389e02fe7e180c836fb72ab0f025
Coral42 <82488154+Coral42@users.noreply.github.com>
2021-04-14 00:27:51 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyAVf1LU1Fe9Mr8--LGy3PGRar5ynifH-wk",
+     apiKey: "AIzaSyAVf1LU1Fe9Mr8--LGy3PGRar5ynifH-wk",
     authDomain: "chat42cs.firebaseapp.com",
     projectId: "chat42cs",
     storageBucket: "chat42cs.appspot.com",
     messagingSenderId: "351775557816",
-    appId: "1:351775557816:web:813c6008f4b1e4e2cb4c4d",
-    measurementId: "G-R7SE0NGVNC"
+    appId: "1:351775557816:web:fa88ed63798564facb4c4d",
+    measurementId: "G-T6HVP5DJJR"
     });
